4053be21eab2569f1d4a248a67fdce39a7873a28
santiago garcia medina <santiago.garcia34530@ucaldas.edu.co>
2026-03-11 14:28:48 -0500

    fix:corrección de usos de variables

--- services/cloudinaryService.js
+++ services/cloudinaryService.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 const streamifier = require('streamifier');
 
 cloudinary.config({
-  cloud_name: 'dnxzvzwn1', 
-  api_key: '862366238996943', 
-  api_secret: 'xAX7oyoMNVBepdJlSAB2wFd1qvg' 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const uploadFile = async (file) => {
